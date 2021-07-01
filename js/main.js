78fc4d524b6c8361fce12876bc2e38aa9c7cdcdf
RomanDyukoff <Hlebahleb@gmail.com>
2021-07-01 15:31:08 +0300

    fixed

--- js/main.js
+++ js/main.js
@@ -169,10 +169,12 @@
 
 const button = document.querySelector('.button');
 const modal = document.querySelector('.modal');
-const close = document.getElementById('.modal__close');
+const close = document.querySelector('.modal__close');
 
 button.addEventListener('click', () => {
   modal.classList.toggle('modal-active');
 });
+
+
 
 
